661b86f4838bea80183cd529a632f5b14ba4b77d
Sarah Van Ness <sv1205@bu.edu>
2019-05-14 16:47:52 -0400

    Added analysis code

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,26 @@
+#Sarah Leavitt
+#BSA Seminar 5/30/19
+
+setwd("~/Boston University/BSA_Seminar")
+library(dplyr)
+library(ggplot2)
+
+
+############### Analysis #################
+
+#Read in the dataset
+irisSetosa <- readRDS("../setosa.rds")
+
+#Plot sepal length vs. petal length
+ggplot(data = irisSetosa, aes(y = Sepal.Length, x = Petal.Length)) +
+  geom_point()
+
+#Linear regression
+m <- lm(Sepal.Length ~ Petal.Length, data = irisSetosa)
+summary(m)
+
+#Plot sepal length vs. petal length with regression line
+ggplot(data = irisSetosa, aes(y = Sepal.Length, x = Petal.Length)) +
+  geom_point() +
+  geom_smooth(method = "lm")
 
