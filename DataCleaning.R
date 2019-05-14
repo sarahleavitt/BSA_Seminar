1d384969774acf6244f0626fc09d6e255efde112
Sarah Van Ness <sv1205@bu.edu>
2019-05-14 16:48:03 -0400

    Added data cleaning code

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,3 +1,20 @@
+#Sarah Leavitt
+#BSA Seminar 5/30/19
 
+setwd("~/Boston University/BSA_Seminar")
+library(dplyr)
+
+
+############### Data Cleaning #################
+
+#Looking at the iris dataset
+str(iris)
+table(iris$Species)
+
+#Subsetting to just the Setosa species
+irisSetosa <- iris %>% filter(Species == "setosa")
+
+#Saving the new dataset
+saveRDS(irisSetosa, "../setosa.rds")
 
 
